1cc219459286a977b36dbccefa86e32d41b11cbb
unknown <s24393@pjwstk.edu.pl>
2024-06-10 15:57:50 +0200

    test pre-commit 2

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 print("Hello World")
 
+
 class Klasa:
     def __init__(self):
         print("Konstruktor")
 
     def metoda(self):
         print("Metoda")
+        print("Test")
+
 Klasa().metoda()
