8a376ad2102c18722d0c5115204e48090a267a85
unknown <s24393@pjwstk.edu.pl>
2024-06-19 14:05:44 +0200

    adds model architecture

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from torch.utils.data import DataLoader
 
 from pathsLoader import load_paths_prisoners_dataset
 from prisonersDataset import PrisonersDataset
+from prisonersModel import PrisonersModel
 from trainingConfig import TrainingConfig
 from unzip import unzip_prisoners_dataset
 
@@ -17,7 +18,13 @@ dataset = PrisonersDataset(paths)
 # Create data loader
 dataloader = DataLoader(dataset=dataset, batch_size=32, shuffle=True)
 # Create model
-
+model = PrisonersModel(
+    sample_size=trainingConfig.image_size,
+    in_channels=3,
+    out_channels=3,
+    layers_per_block=2,
+    block_out_channels=(128, 128, 256, 256, 512, 512),
+).getModel()
 # Create noise scheduler
 
 # Train and save model
---
+++ prisonersModel.py
@@ -0,0 +1,49 @@
+import diffusers.models
+from diffusers import DDPMPipeline, UNet2DModel
+from diffusers.utils import make_image_grid
+
+
+class PrisonersModel:
+    """
+    This class creates model to training
+
+    """
+
+    def __init__(
+        self,
+        sample_size: int,
+        in_channels: int,
+        out_channels: int,
+        layers_per_block: int,
+        block_out_channels: tuple,
+    ) -> None:
+        self.model = UNet2DModel(
+            sample_size=sample_size,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            layers_per_block=layers_per_block,
+            block_out_channels=block_out_channels,
+            down_block_types=(
+                "DownBlock2D",
+                "DownBlock2D",
+                "DownBlock2D",
+                "DownBlock2D",
+                "AttnDownBlock2D",
+                "DownBlock2D",
+            ),
+            up_block_types=(
+                "UpBlock2D",
+                "AttnUpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+            ),
+        )
+
+    def getModel(self) -> diffusers.models.unets.UNet2DModel:
+        """
+        Simple getter.
+        :return: unet2dmodel
+        """
+        return self.model
