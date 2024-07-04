6ab60627a0ca84d31cd593487f041cb6016fd456
KacperKnitter <knitterkacper@gmail.com>
2024-07-05 00:39:29 +0200

    added docmumentation to Schedulder class

--- noiseScheduler.py
+++ noiseScheduler.py
@@ -5,6 +5,19 @@ from prisonersDataset import PrisonersDataset
 
 
 class Scheduler:
+    """
+    Scheduler class for the noise.
+
+    Attributes:
+    - noise_scheduler: the noise scheduler.
+    - noise: the noise tensor.
+    - timesteps: the timesteps.
+
+    Methods:
+    - __init__: initializes the scheduler.
+
+    """
+
     def __init__(self, sample_image: torch.Tensor, lr_warmup_steps: int) -> None:
 
         self.noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
