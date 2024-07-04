80c86829546837ebf4c62e0f41458da0b7ed2818
KacperKnitter <knitterkacper@gmail.com>
2024-07-05 00:45:00 +0200

    updated docmumentation for noiseSchedulder file

--- noiseScheduler.py
+++ noiseScheduler.py
@@ -19,6 +19,16 @@ class Scheduler:
     """
 
     def __init__(self, sample_image: torch.Tensor, lr_warmup_steps: int) -> None:
+        """
+        Initializes the scheduler.
+
+        Args:
+        - sample_image (Tensor): the sample image.
+        - lr_warmup_steps (int): the learning rate warmup steps.
+
+        Returns:
+        - None
+        """
 
         self.noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
         self.noise = torch.randn(sample_image.shape)
